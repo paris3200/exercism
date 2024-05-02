ed2e3fbe0b048ae3f0697a9ccfc6e2ca7085fdf6
Jason Paris <paris3200@gmail.com>
2024-05-02 18:52:35 -0400

    WIP

---
+++ javascript/elyses-analytic-enchantments/enchantments.js
@@ -0,0 +1,76 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Get the position (index) of the card in the given stack
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} position of the card in the stack
+ */
+export function getCardPosition(stack, card) {
+  return stack.indexOf(card)
+}
+
+/**
+ * Determine if the stack contains the card
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {boolean} true if card is in the stack, false otherwise
+ */
+export function doesStackIncludeCard(stack, card) {
+  if (getCardPosition(stack, card) > -1){
+    return true
+  }
+  return false
+}
+
+/**
+ * Determine if each card is even
+ *
+ * @param {number[]} stack
+ *
+ * @returns {boolean} true if all cards are even, false otherwise
+ */
+export function isEachCardEven(stack) {
+  throw new Error('Implement the isEachCardEven function');
+}
+
+/**
+ * Check if stack contains odd-value card
+ *
+ * @param {number[]} stack
+ *
+ * @returns {boolean} true if the array contains odd card, false otherwise
+ */
+export function doesStackIncludeOddCard(stack) {
+  throw new Error('Implement the doesStackIncludesOddCard function');
+}
+
+/**
+ * Get the first odd card from the stack
+ *
+ * @param {number[]} stack
+ *
+ * @returns {number | undefined} the first odd value
+ */
+export function getFirstOddCard(stack) {
+  throw new Error('Implement the getFirstOddCard function');
+}
+
+/**
+ * Determine the position of the first card that is even
+ *
+ * @param {number[]} stack
+ *
+ * @returns {number} position of the first card that is even
+ */
+export function getFirstEvenCardPosition(stack) {
+  throw new Error('Implement the getFirstEvenCardPosition function');
+}
